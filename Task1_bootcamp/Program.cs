aeba6c00561bf663e70fddfc345a3194266eb7eb
Rf <101931924+Rifaaauzh@users.noreply.github.com>
2026-03-31 19:13:06 +0700

    Update Program.cs

--- Task1_bootcamp/Program.cs
+++ Task1_bootcamp/Program.cs
@@ -4,17 +4,17 @@ int userInput = int.Parse(input);
 
 for (int i = 1; i <= userInput; i++)
 {
-    if (i % 3 == 0)
+    if (i % 3 == 0 && i % 5 == 0 )
+    {
+        Console.Write("foobar");
+    }
+    else if (i % 3 == 0)
     {
         Console.Write("foo");
     }
     else if (i % 5 == 0)
     {
         Console.Write("bar");
-    }
-    else if (i % 3 == 0 && i % 5 == 0 )
-    {
-        Console.Write("foobar");
     }
     else
     {
